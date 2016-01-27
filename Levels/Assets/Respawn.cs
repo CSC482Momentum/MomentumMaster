5ec8c64a76d89f0256b7a3787a0956ac87bf1a1f
Jordan <jlfurgeson@gmail.com>
2016-01-27 17:05:06 -0500

    Initial Project

---
+++ Levels/Assets/Respawn.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Respawn : MonoBehaviour
+{
+    public GameObject prefabPe;
+    public Vector3 Spawn;
+    public Quaternion Rotation;
+    
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            col.transform.position = Spawn;
+//          col.transform.localEulerAngles = Rotation;
+            Instantiate(prefabPe, col.transform.position, Rotation);
+
+            Destroy(prefabPe, 2);
+
+ //           col.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY;
+ //           new WaitForSeconds(1);
+//            col.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+        }
+    }
+}
---
+++ Levels/Assets/Standard Assets/Characters/FirstPersonCharacter/Prefabs/Clickable.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Clickable : MonoBehaviour {
+
+    public RaycastHit hit;
+    public Ray ray;
+    private Vector3 touchpos;
+
+    public float radius = 5.0f;
+    public float power = 10.0f;
+
+    void Update ()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+//            if (Physics.Raycast(ray, out hit, 1000))
+//            {
+                ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                touchpos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+                Vector3 dir = gameObject.transform.position - touchpos;
+                hit.transform.SendMessage("Touched", dir);
+//            }
+        }
+    }
+	
+}
+

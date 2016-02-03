7115691ff63201ba0bf4569bb6cabba407dce944
Jordan <jlfurgeson@gmail.com>
2016-02-03 17:28:13 -0500

    Can switch weapons with '1' and '2', not on numberpad. Gun points in direction of camera. Push and pull scripts attached to their corresponding guns now.

---
+++ Levels/Assets/PullScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class PullScript : MonoBehaviour
+{
+
+    public float pullforce;
+    public float pullplayer;
+    public RigidbodyFirstPersonController fpsc;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            RaycastHit hit;
+    //        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+            Vector3 fwd = transform.parent.transform.TransformDirection(Vector3.forward);
+            fwd = fwd.normalized;
+            if (Physics.Raycast(transform.position, fwd, out hit, 20))
+            {
+                if (hit.collider.tag == "Hook")
+                {
+                    print("hit3!");
+                    fpsc.m_Jump = true;
+                    this.transform.parent.transform.parent.GetComponent<Rigidbody>().AddForce((hit.transform.position - transform.position).normalized * pullplayer);
+
+                }
+                else
+                {
+                    if (hit.rigidbody != null)
+                    {
+                        print("hit2!");
+                        hit.rigidbody.AddForce((-fwd) * pullforce);
+                    }
+                }
+            }
+        }
+    }
+
+
+}
+

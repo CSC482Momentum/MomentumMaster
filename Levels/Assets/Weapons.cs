092c8130de08ed7e162b8cb048ef3198d5cd40f7
Jordan <jlfurgeson@gmail.com>
2016-02-04 12:06:56 -0500

    Just some updates.

---
+++ Levels/Assets/Weapons.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapons : MonoBehaviour {
+
+    public int currentweapon = 0;
+    public GameObject[] weapons;
+    private int nrWeapons;
+
+	// Use this for initialization
+	void Start () {
+        nrWeapons = weapons.Length;
+        SwitchWeapon(currentweapon);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    for (int i=1; i <= nrWeapons; i++)
+        {
+            if (Input.GetKeyDown("" + i))
+            {
+                currentweapon = i - 1;
+                SwitchWeapon(currentweapon);
+            }
+        }
+	}
+
+    void SwitchWeapon(int index)
+    {
+        for (int i=0; i < nrWeapons; i++)
+        {
+            if (i == index)
+            {
+                weapons[i].gameObject.SetActive(true);
+            } else
+            {
+                weapons[i].gameObject.SetActive(false);
+            }
+        }
+    }
+}

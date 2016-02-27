710e855229599ab016f20064403de891ae45bf7e
Evin Clapper <efclappe@ncsu.edu>
2016-02-27 15:37:42 -0500

    Main Menu scene to be hooked up

---
+++ Momentum/Assets/SceneChanger.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour {
+
+
+	public void loadTutorial() {
+		SceneManager.LoadScene ("Tutorial");
+	}
+
+	public void loadCubedDonut() {
+		SceneManager.LoadScene ("CubedDonut");
+	}
+
+	public void loadQuit() {
+		Application.Quit ();
+	}
+		
+}
--- Momentum/Assets/Scripts/WorldController.cs
+++ Momentum/Assets/Scripts/WorldController.cs
@@ -5,4 +5,11 @@ public class WorldController : MonoBehaviour {
 
 	public AudioManager audioManager;
 	public AnimationManager animationManager;
+	public SceneChanger sceneChanger;
+
+	private string currentLang = "English";
+
+	void Awake() {
+		DontDestroyOnLoad(transform.gameObject);
+	}
 }

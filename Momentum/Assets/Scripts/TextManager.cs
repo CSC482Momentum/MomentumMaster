27e5a8da563a6733208d5f5d58e9580fd675b8a2
Evin Clapper <efclappe@ncsu.edu>
2016-02-27 14:29:18 -0500

    Canvas prefab:  Contains 3 menus (main, language, pause) right now the options button on the main menu just takes you to language selection because i don't know what other "options" we have defined anyway besides maybe volume?  Languages menu works and translates the main and pause menu correctly.

--- Momentum/Assets/Scripts/TextManager.cs
+++ Momentum/Assets/Scripts/TextManager.cs
@@ -28,38 +28,45 @@ public class TextManager : MonoBehaviour {
 	public void setLanguage(string targetLang) {
 		switch (targetLang) {
 		case "English":
-			for(int i = 0; i < textFields.Length; i++) {
-				textFields[i].text = englishText[i];
+			for(int i = 0; i <= textFields.Length - 2; i+=2) {
+				textFields[i].text = englishText[i/2];
+				textFields[i+1].text = englishText[i/2];
 			}
 			break;
 		case "Spanish":
-			for(int i = 0; i < textFields.Length; i++) {
-				textFields[i].text = spanishText[i];
+			for(int i = 0; i < textFields.Length; i+=2) {
+				textFields[i].text = spanishText[i/2];
+				textFields[i+1].text = spanishText[i/2];
 			}
 			break;
 		case "French":
-			for(int i = 0; i < textFields.Length; i++) {
-				textFields[i].text = frenchText[i];
+			for(int i = 0; i < textFields.Length; i+=2) {
+				textFields[i].text = frenchText[i/2];
+				textFields[i+1].text = frenchText[i/2];
 			}
 			break;
 		case "German":
-			for(int i = 0; i < textFields.Length; i++) {
-				textFields[i].text = germanText[i];
+			for(int i = 0; i < textFields.Length; i+=2) {
+				textFields[i].text = germanText[i/2];
+				textFields[i+1].text = germanText[i/2];
 			}
 			break;
 		case "Italian":
-			for(int i = 0; i < textFields.Length; i++) {
-				textFields[i].text = italianText[i];
+			for(int i = 0; i < textFields.Length; i+=2) {
+				textFields[i].text = italianText[i/2];
+				textFields[i+1].text = italianText[i/2];
 			}
 			break;
 		case "Japanese":
-			for(int i = 0; i < textFields.Length; i++) {
-				textFields[i].text = japaneseText[i];
+			for(int i = 0; i < textFields.Length; i+=2) {
+				textFields[i].text = japaneseText[i/2];
+				textFields[i+1].text = japaneseText[i/2];
 			}
 			break;
 		case "Russian":
-			for(int i = 0; i < textFields.Length; i++) {
-				textFields[i].text = russianText[i];
+			for(int i = 0; i < textFields.Length; i+=2) {
+				textFields[i].text = russianText[i/2];
+				textFields[i+1].text = russianText[i/2];
 			}
 			break;
 		}

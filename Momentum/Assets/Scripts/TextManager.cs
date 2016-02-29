5c2c78ea9bcae6b2d168c86a1a3f674e772dd24f
Evin Clapper <efclappe@ncsu.edu>
2016-02-29 11:39:05 -0500

    Pause Menu is in, however it is disabled for now because I need to research a way to block input while it is up.  Also added translations for the pause menu and implemented a main menu (where only host, quit, and options work for now).

--- Momentum/Assets/Scripts/TextManager.cs
+++ Momentum/Assets/Scripts/TextManager.cs
@@ -20,9 +20,12 @@ public class TextManager : MonoBehaviour {
 
 	public string[] russianText;
 
+	public WorldController worldController;
+
 	// Use this for initialization
 	void Start () {
 		setLanguage ("English");
+		worldController = WorldController.getInstance ();
 	}
 
 	public void setLanguage(string targetLang) {
@@ -31,42 +34,50 @@ public class TextManager : MonoBehaviour {
 			for(int i = 0; i <= textFields.Length - 2; i+=2) {
 				textFields[i].text = englishText[i/2];
 				textFields[i+1].text = englishText[i/2];
+				worldController.setCurrentLanguage ("English");
+
 			}
 			break;
 		case "Spanish":
 			for(int i = 0; i < textFields.Length; i+=2) {
 				textFields[i].text = spanishText[i/2];
 				textFields[i+1].text = spanishText[i/2];
+				worldController.setCurrentLanguage ("Spanish");
 			}
 			break;
 		case "French":
 			for(int i = 0; i < textFields.Length; i+=2) {
 				textFields[i].text = frenchText[i/2];
 				textFields[i+1].text = frenchText[i/2];
+				worldController.setCurrentLanguage ("French");
 			}
 			break;
 		case "German":
 			for(int i = 0; i < textFields.Length; i+=2) {
 				textFields[i].text = germanText[i/2];
 				textFields[i+1].text = germanText[i/2];
+				worldController.setCurrentLanguage ("German");
 			}
 			break;
 		case "Italian":
 			for(int i = 0; i < textFields.Length; i+=2) {
 				textFields[i].text = italianText[i/2];
 				textFields[i+1].text = italianText[i/2];
+				worldController.setCurrentLanguage ("Italian");
 			}
 			break;
 		case "Japanese":
 			for(int i = 0; i < textFields.Length; i+=2) {
 				textFields[i].text = japaneseText[i/2];
 				textFields[i+1].text = japaneseText[i/2];
+				worldController.setCurrentLanguage ("Japanese");
 			}
 			break;
 		case "Russian":
 			for(int i = 0; i < textFields.Length; i+=2) {
 				textFields[i].text = russianText[i/2];
 				textFields[i+1].text = russianText[i/2];
+				worldController.setCurrentLanguage ("Russian");
 			}
 			break;
 		}
--- Momentum/Assets/Scripts/WorldController.cs
+++ Momentum/Assets/Scripts/WorldController.cs
@@ -5,6 +5,16 @@ public class WorldController : MonoBehaviour {
     private static WorldController instance;
     public AudioManager audioManager;
 	public AnimationManager animationManager;
+
+	public GameObject hudCanvas;
+	public GameObject optionsMenu;
+
+	//Pause handler
+	public bool showingOptions;
+	public bool isPaused = false;
+	public bool pDown = false;
+	private bool pUp = false;
+
     public static WorldController getInstance()
     {
         if (instance == null)
@@ -21,5 +31,54 @@ public class WorldController : MonoBehaviour {
 	void Awake() {
 		DontDestroyOnLoad(transform.gameObject);
 	}
+
+	void OnLevelWasLoaded(int level) {
+		if(level == 1) {
+			//optionsMenu.GetComponent<TextManager> ().setLanguage (currentLang);
+		}
+	}
+
+	public void setCurrentLanguage(string newLang) {
+		currentLang = newLang;
+	}
+//	void Update() {
+//		if (Input.GetKeyDown (KeyCode.P)) {
+//			if (!isPaused) {
+//				if (hudCanvas != null) {
+//					if (!pDown) {
+//						for (int i = 0; i < hudCanvas.transform.childCount; i++) {
+//							bool isPauseMenu = false;
+//							if (hudCanvas.transform.GetChild (i).name == "InGameMenu") {
+//								isPauseMenu = true;
+//							}
+//							hudCanvas.transform.GetChild (i).gameObject.SetActive (isPauseMenu);
+//						}
+//					} else {
+//						pDown = true;
+//					}
+//				}
+//				isPaused = true;
+//			} else if (isPaused && !optionsMenu.activeInHierarchy) {
+//				if (hudCanvas != null) {
+//					if (!pDown) {
+//						for (int i = 0; i < hudCanvas.transform.childCount; i++) {
+//							bool isPauseMenu = false;
+//							if (hudCanvas.transform.GetChild (i).name == "InGameMenu") {
+//								isPauseMenu = true;
+//							}
+//							hudCanvas.transform.GetChild (i).gameObject.SetActive (!isPauseMenu);
+//						}
+//					} else {
+//						pDown = true;
+//					}
+//				}
+//				optionsMenu.SetActive(false);
+//				isPaused = false;
+//			}
+//			if (Input.GetKeyUp (KeyCode.P)) {
+//				pDown = false;
+//			}
+//		}
+//	}
 
 }

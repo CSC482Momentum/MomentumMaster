e88a29fff38f6d9c2f6bd697f7114bdeb11045cf
Derek Batts <clone101.derek@gmail.com>
2016-02-29 15:47:55 -0500

    Added dummy to player prefab

---
+++ Momentum/Assets/modelUpdater.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class modelUpdater : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Translate(this.transform.parent.position);
+        this.transform.Rotate(this.transform.parent.rotation.eulerAngles);
+	}
+}

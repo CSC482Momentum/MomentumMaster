5f33948696a10eafcdb7d9104bfb1ef9440b02a2
bjcollin <bjcollin@ncsu.edu>
2016-02-29 02:15:52 -0500

    Multiplayer! At least a very hacky, ducktaped version of it

---
+++ Momentum/Assets/Scripts/Multiplayer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class Multiplayer : NetworkBehaviour {
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (isLocalPlayer) return; //Do not execute on client
+        GameObject pl = GameObject.FindWithTag("Player");
+
+        if (pl != null) {
+            for (int i = 1; i < 5; i++) {
+                bool filled = GameObject.FindWithTag("Player" + i) != null;
+                if (!filled) {
+                    pl.tag = "Player" + i;
+                    print("Player's NetworkID: " + pl.GetComponent<NetworkIdentity>().netId);
+                    print("Assigned Player " + i);
+                    break;
+                }
+            }
+        }
+    }
+}

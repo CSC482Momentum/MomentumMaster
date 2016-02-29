9fb4c5a1cbf647f602a91fed7a5827189ed98dd1
plmills <plmills@ncsu.edu>
2016-02-28 21:14:07 -0500

    Added a new level, test level, and a ton of meshes and materials.

---
+++ Momentum/Assets/plmills/Grass/GrassCreator.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrassCreator : MonoBehaviour {
+
+	public GameObject grassSprite;
+
+	public float density = 1;
+
+	public int width;
+
+	public int height;
+
+	void Start () {
+
+		//grassSprite.GetComponent<SpriteRenderer>().color = Color.black;
+
+		//BoxCollider boxCollider = (BoxCollider)GetComponent(typeof(BoxCollider));
+
+		Vector3 positional = transform.position;
+
+		//positional.y += .2f;
+
+		for(int i = 0; i < width; i++){
+
+			positional.z = transform.position.z + density * i;
+
+			positional.x = transform.position.x;
+
+			for(int j = 0; j < height; j++){
+				
+				positional.x += density;
+
+				Instantiate(grassSprite, positional, transform.rotation);
+			}
+		}
+
+		Object.Destroy(this.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Momentum/Assets/plmills/mindmillRotate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mindmillRotate : MonoBehaviour {
+
+	public float rotationSpeed = 14;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.back * Time.deltaTime * rotationSpeed);
+	}
+}

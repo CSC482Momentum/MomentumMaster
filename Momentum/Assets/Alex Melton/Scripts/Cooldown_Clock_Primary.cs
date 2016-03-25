74f6d7c69576435b26bb9aeda8a94efb8c3a3a37
Alex Sawicki <alex@kb-promotions.net>
2016-03-25 11:44:59 -0400

    UI should now be functioning correctly. It should be correctly reflect the cooldowns on the UI.

--- Momentum/Assets/Alex Melton/Scripts/Cooldown_Clock_Primary.cs
+++ Momentum/Assets/Alex Melton/Scripts/Cooldown_Clock_Primary.cs
@@ -4,41 +4,58 @@ using System.Collections;
 using Assets;
 using UnityStandardAssets.CrossPlatformInput;
 
-public class Cooldown_Clock_Primary : MonoBehaviour {
+public class Cooldown_Clock_Primary : MonoBehaviour
+{
 
-	private Image image;
-	private bool isCoolingDown = false;
+    private Image image;
+    private bool isCoolingDown = false;
     private float coolDownTime;
     public Weapon weap;
 
-	// Initialize myself
-	void Awake () {
-		image = gameObject.GetComponent<Image> ();
-        this.coolDownTime = weap.getSecondaryCooldown();
-       // print(coolDownTime);
-	}
+    // Initialize myself
+    void Awake()
+    {
+        image = gameObject.GetComponent<Image>();
+        //this.coolDownTime = weap.getSecondaryCooldown();
+        // print(coolDownTime);
+    }
 
-	// Initialize relationship to others
-	void Start () {
-		//null
-	}
+    // Initialize relationship to others
+    void Start()
+    {
+        //null
+    }
 
-	// Update is called once per frame
-	void Update () {
-        if (weap.isActiveAndEnabled && !isCoolingDown && CrossPlatformInputManager.GetButtonDown("Fire1") || Input.GetAxisRaw("Xbox Right Trigger") != 0)
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (weap.isActiveAndEnabled && !isCoolingDown && CrossPlatformInputManager.GetButtonDown("Fire1") || Input.GetAxisRaw("Xbox Right Trigger") != 0)
         {
             isCoolingDown = true;
         }
-        UpdateValue ();
-	}
+        UpdateValue ();*/
+        if (weap.primaryCoolingDown)
+        {
+            image.fillAmount -= 1.0f / weap.getPrimaryCooldown() * Time.deltaTime;
+        }
+        else
+        {
+            image.fillAmount = 1;
+        }
+        if (image.fillAmount <= 0)
+        {
+            image.fillAmount = 1;
+        }
+    }
 
-	void UpdateValue () {
+    /*void UpdateValue () {
 		if (isCoolingDown == true) {
 			image.fillAmount -= 1.0f/coolDownTime * Time.deltaTime;
 		}
-		if (image.fillAmount == 0) { // this means that we JUST finshed cooling down.
+		if (image.fillAmount <= 0) { // this means that we JUST finshed cooling down.
 			isCoolingDown = false;
 			image.fillAmount = 1;
 		}
-	}
+
+	}*/
 }
--- Momentum/Assets/Alex Melton/Scripts/Cooldown_Clock_Secondary.cs
+++ Momentum/Assets/Alex Melton/Scripts/Cooldown_Clock_Secondary.cs
@@ -4,35 +4,51 @@ using System.Collections;
 using Assets;
 using UnityStandardAssets.CrossPlatformInput;
 
-public class Cooldown_Clock_Secondary : MonoBehaviour {
+public class Cooldown_Clock_Secondary : MonoBehaviour
+{
 
-	private Image image;
-	private bool isCoolingDown = false;
+    private Image image;
+    private bool isCoolingDown = false;
     private float coolDownTime;
     public Weapon weap;
 
-	// Initialize myself
-	void Awake () {
-		image = gameObject.GetComponent<Image> ();
-        this.coolDownTime = weap.getPrimaryCooldown();
-       // print(coolDownTime);
-	}
+    // Initialize myself
+    void Awake()
+    {
+        image = gameObject.GetComponent<Image>();
+        //this.coolDownTime = weap.getPrimaryCooldown();
+        // print(coolDownTime);
+    }
 
-	// Initialize relationship to others
-	void Start () {
-		//null
-	}
+    // Initialize relationship to others
+    void Start()
+    {
+        //null
+    }
 
-	// Update is called once per frame
-	void Update () {
-        if (weap.isActiveAndEnabled && !isCoolingDown && CrossPlatformInputManager.GetButtonDown("Fire2") || Input.GetAxisRaw("Xbox Left Trigger") != 0)
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (weap.isActiveAndEnabled && !isCoolingDown && CrossPlatformInputManager.GetButtonDown("Fire2") || Input.GetAxisRaw("Xbox Left Trigger") != 0)
         {
             isCoolingDown = true;
         }
-        UpdateValue ();
-	}
+        UpdateValue ();*/
+        if (weap.secondaryCoolingDown)
+        {
+            image.fillAmount -= 1.0f / weap.getSecondaryCooldown() * Time.deltaTime;
+        }
+        else
+        {
+            image.fillAmount = 1;
+        }
+        if (image.fillAmount <= 0)
+        {
+            image.fillAmount = 1;
+        }
+    }
 
-	void UpdateValue () {
+    /*void UpdateValue () {
 		if (isCoolingDown == true) {
 			image.fillAmount -= 1.0f/coolDownTime * Time.deltaTime;
 		}
@@ -40,5 +56,5 @@ public class Cooldown_Clock_Secondary : MonoBehaviour {
 			isCoolingDown = false;
 			image.fillAmount = 1;
 		}
-	}
+	}*/
 }
